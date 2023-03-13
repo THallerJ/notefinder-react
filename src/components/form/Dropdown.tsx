3e5cdc22a16b0982aa95160c2239e82666b86507
Thomas Haller <hallert60@gmail.com>
2023-03-12 22:47:43 -0500

    - Fixed bug where Dropdown component opens when hovering over the invisible Dropdown content

--- src/components/form/Dropdown.tsx
+++ src/components/form/Dropdown.tsx
@@ -7,12 +7,12 @@ type DropdownProps = {
 
 const Dropdown = ({ button, content, left, right }: DropdownProps) => {
   return (
-    <div className="group relative inline-block text-left">
-      {button}
+    <div className="relative inline-block text-left">
+      <div className="peer">{button}</div>
       <div
-        className={`absolute z-10 mt-2 min-w-[6rem] bg-gray-700 p-4 opacity-0 
+        className={`absolute z-50 mt-2 min-w-[6rem] bg-gray-700 p-4 opacity-0 
         shadow-lg ring-1 ring-inset ring-gray-900 transition-opacity duration-300 
-        group-focus-within:opacity-100 group-hover:opacity-100 
+        peer-focus-within:opacity-100 peer-hover:opacity-100 
         ${right && 'right-0'} ${left && 'left-0'}${
           !left && !right && 'left-1/2 -translate-x-1/2'
         }`}
