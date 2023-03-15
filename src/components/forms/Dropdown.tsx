07286922e92a69ae9a1146a199e47cd381d26f9d
Thomas Haller <hallert60@gmail.com>
2023-03-14 23:03:25 -0500

    - Dropdown now has black ring color

--- src/components/forms/Dropdown.tsx
+++ src/components/forms/Dropdown.tsx
@@ -11,7 +11,7 @@ const Dropdown = ({ button, content, left, right }: DropdownProps) => {
       {button}
       <div
         className={`absolute z-10 mt-0 min-w-[6rem] scale-0 bg-[#3c3c3c]
-          p-4 shadow-lg ring-1 ring-inset ring-gray-900 duration-150 
+          p-4 shadow-lg ring-1 ring-inset ring-black duration-150 
           ease-in-out group-focus-within:scale-100 group-hover:scale-100
           ${right && 'right-0 origin-top-right'} 
           ${left && 'left-0 origin-top-left'} 
